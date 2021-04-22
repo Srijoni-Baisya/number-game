f72994f2388358b4e6bc7f88e916e5618c8fb036
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-04-22 11:32:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ function check(){
       document.getElementById("output").innerHTML="It is an odd number";
    }
 }
+function verify(){
+   get_whole_number = document.getElementById("whole_no").value;
+   whole_number = parseInt(get_whole_number);
+   console.log(whole_number);
+   var factors = [];
+
+   for(i=1;i<=whole_number;i++){
+      if(whole_number%i==0){
+         factors.push(i);
+      }
+   }
+   console.log(factors);
+  
+   length_of_array = factors.length;
+   console.log(length_of_array);
+
+   if(length_of_array==2){
+      console.log("prime");
+      document.getElementById("info").style.display="inline-block";
+      document.getElementById("info").innerHTML="It is a prime number";
+   }
+   else{
+      console.log("composite");
+      document.getElementById("info").style.display="inline-block";
+      document.getElementById("info").innerHTML="It is a composite number";
+   }
+}
